ef734eff06506e7575a0165401e3af05aaccfd21
lexed <lexed@BD-02-12>
2020-09-28 16:18:43 +0800

    主干

--- src/main/java/cn/kgc/demo.java
+++ src/main/java/cn/kgc/demo.java
@@ -4,7 +4,12 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ssss");
+		System.out.println("hello word");
+		System.out.println("hello a");
+		System.out.println("hello b");
+		System.out.println("分支项目");
+		System.out.println("qweqeq");
+		System.out.println("主干添加");
 	}
 
 }
