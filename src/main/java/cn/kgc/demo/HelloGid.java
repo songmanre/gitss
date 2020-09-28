d6378ca6d767eb31e3f56385b6764c3835c4820f
lexed <lexed@BD-02-12>
2020-09-28 14:58:00 +0800

    work

---
+++ src/main/java/cn/kgc/demo/HelloGid.java
@@ -0,0 +1,23 @@
+package cn.kgc.demo;
+
+public class HelloGid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		package cn.kgc.word;
+
+		public class HelloGid {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				System.out.println("Hello Git！");
+				System.out.println("11111111111");
+				System.out.println("22222222222");
+				
+			}
+
+		}
+
+	}
+
+}
